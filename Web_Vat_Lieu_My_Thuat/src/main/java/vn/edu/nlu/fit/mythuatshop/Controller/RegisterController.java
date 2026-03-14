d8be9a42b9ef34e11ba620abc307455fdb2cd853
Hieu <23130102@st.hcmuaf.edu.vn>
2026-03-14 22:21:08 +0700

    sửa giao diện chức năng đăng ký

--- Web_Vat_Lieu_My_Thuat/src/main/java/vn/edu/nlu/fit/mythuatshop/Controller/RegisterController.java
+++ Web_Vat_Lieu_My_Thuat/src/main/java/vn/edu/nlu/fit/mythuatshop/Controller/RegisterController.java
@@ -1,6 +1,5 @@
 package vn.edu.nlu.fit.mythuatshop.Controller;
 
-import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -14,10 +13,9 @@ import java.io.IOException;
 public class RegisterController extends HttpServlet {
     private UserService userService = new UserService();
 
-
     @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
+    public void init() {
+        userService = new UserService();
     }
 
     @Override
@@ -27,22 +25,21 @@ public class RegisterController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      req.setCharacterEncoding("UTF-8");
-      resp.setCharacterEncoding("UTF-8");
+        req.setCharacterEncoding("UTF-8");
 
-      String fullName = req.getParameter("fullName");
-      String email = req.getParameter("email");
-      String password = req.getParameter("password");
-      String phoneNumber = req.getParameter("phoneNumber");
-      String address = req.getParameter("address");
-//        check error sdt
+        String fullName = req.getParameter("fullName");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String phoneNumber = req.getParameter("phoneNumber");
+        String address = req.getParameter("address");
+
         if (phoneNumber == null || !phoneNumber.matches("^0\\d{9}$")) {
             req.setAttribute("error", "Số điện thoại không hợp lệ (vd: 0912345678)");
             req.getRequestDispatcher("Register.jsp").forward(req, resp);
             return;
         }
 
-//        check error mk
+
         if(password==null || !password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{8,}$")){
             req.setAttribute("error", "Mật khẩu có ít nhất 8 kí tự gồm chữ hoa, chũ thường và các kí tự đặc biệt.");
             req.getRequestDispatcher("Register.jsp").forward(req, resp);
@@ -54,13 +51,12 @@ public class RegisterController extends HttpServlet {
                 + req.getContextPath();
 
         boolean success = userService.register(fullName, email, phoneNumber, password, address, baseUrl);
-      if(!success){
-          req.setAttribute("error", "Email đã được sử dụng, vui lòng nhập email khác!");
-          req.getRequestDispatcher("Register.jsp").forward(req,resp);
-          return;
-      }
+        if(!success){
+            req.setAttribute("error", "Email đã được sử dụng, vui lòng nhập email khác!");
+            req.getRequestDispatcher("Register.jsp").forward(req,resp);
+            return;
+        }
 
-//            resp.sendRedirect(req.getContextPath() + "/register?success=true");
         resp.sendRedirect(req.getContextPath() + "/register?success=true&verify=sent");
 
     }
