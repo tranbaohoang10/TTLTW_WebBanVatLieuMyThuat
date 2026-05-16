5f846806a0f2b7472434cab27e27bb74c2d207b4
Hieu <23130102@st.hcmuaf.edu.vn>
2026-05-16 11:47:44 +0700

    viết Dao, model, service lấy các nhóm quyền từ db

---
+++ Web_Vat_Lieu_My_Thuat/src/main/java/vn/edu/nlu/fit/mythuatshop/Dao/GroupRoleDao.java
@@ -0,0 +1,21 @@
+package vn.edu.nlu.fit.mythuatshop.Dao;
+
+import org.jdbi.v3.core.Jdbi;
+import vn.edu.nlu.fit.mythuatshop.Model.GroupRole;
+
+import java.util.List;
+
+public class GroupRoleDao {
+    private final Jdbi jdbi;
+    public GroupRoleDao() {
+        this.jdbi = JDBIConnector.getJdbi();
+    }
+    public List<GroupRole> findAll() {
+        String sql = "select id, code, case when code = 'ADMIN_GROUP' then 'Nhóm quản trị'" +
+                                            "when code = 'STAFF_GROUP' then 'Nhóm nhân viên'" +
+                                            "when code = 'USER_GROUP' then 'Nhóm khách hàng'" +
+                                            "else as name from group_role" +
+                                            "order by id";
+        return  jdbi.withHandle(handle -> handle.createQuery(sql).mapToBean(GroupRole.class).list());
+    }
+}
