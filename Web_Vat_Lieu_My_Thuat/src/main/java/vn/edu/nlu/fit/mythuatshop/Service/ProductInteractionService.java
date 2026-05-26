66188944fa03b4ea82cc6769bc2f832bdfa8018d
danghau2512 <23130095@st.hcmuaf.edu.vn>
2026-05-26 23:07:54 +0700

    Thêm chức năng xuất dữ liệu train gợi ý sản phẩm

--- Web_Vat_Lieu_My_Thuat/src/main/java/vn/edu/nlu/fit/mythuatshop/Service/ProductInteractionService.java
+++ Web_Vat_Lieu_My_Thuat/src/main/java/vn/edu/nlu/fit/mythuatshop/Service/ProductInteractionService.java
@@ -27,4 +27,18 @@ public class ProductInteractionService {
     public List<ProductTrainData> getTrainingData() {
         return interactionDao.getTrainingData();
     }
+    public String exportTrainingDataCsv() {
+        List<ProductTrainData> dataList = getTrainingData();
+
+        StringBuilder csv = new StringBuilder();
+        csv.append("userID,productID,score\n");
+
+        for (ProductTrainData data : dataList) {
+            csv.append(data.getUserId()).append(",");
+            csv.append(data.getProductId()).append(",");
+            csv.append(data.getScore()).append("\n");
+        }
+
+        return csv.toString();
+    }
 }
